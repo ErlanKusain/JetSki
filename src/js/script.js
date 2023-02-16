b077ca0e27d4064f4c31acca198cb73660935bbc
erlankusain <erlankusain@yandex.ru>
2023-02-16 23:17:34 +0600

    slick slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.promo__slider').slick({
+      speed: 1500,
+      dots: true,
+      adaptiveHeight:true,
+      prevArrow: '<button type="button" class="slick-prev"><img src= "../img/icons/left-arrow.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src= "../img/icons/right-arrow.svg"></button>'
+    });
+  });
