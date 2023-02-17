e796c5afa79bc05e14778ec302a23413d592cbbe
erlankusain <erlankusain@yandex.ru>
2023-02-17 17:26:25 +0600

    promo section

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.promo__slider').slick({
-      speed: 1500,
+      speed: 1200,
       dots: true,
       adaptiveHeight:true,
       prevArrow: '<button type="button" class="slick-prev"><img src= "../img/icons/left-arrow.svg"></button>',
