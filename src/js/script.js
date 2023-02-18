51324f170ee591ddd0c720c3bbdf2523be87d753
erlankusain <erlankusain@yandex.ru>
2023-02-18 10:40:52 +0600

    search section

--- src/js/script.js
+++ src/js/script.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
       prevArrow: '<button type="button" class="slick-prev"><img src= "../img/icons/left-arrow.svg"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src= "../img/icons/right-arrow.svg"></button>'
     });
+
+    $('ul.search__tabs').on('click', 'li:not(.search__tab_active)', function() {
+      $(this)
+        .addClass('search__tab_active').siblings().removeClass('search__tab_active')
+        .closest('div.container').find('div.search__content').removeClass('search__content_active').eq($(this).index()).addClass('search__content_active');
+    });
+
   });
